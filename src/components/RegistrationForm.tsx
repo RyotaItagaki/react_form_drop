a30c0c8d994fb1055b2fcf1e17589a0d8a5a17bd
Ryota Itagaki <ribitamana@gmail.com>
2022-11-26 17:57:09 +0900

    React Hook Formでフォームの入力内容をコンソールに出力

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,21 +1,40 @@
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-props-no-spreading */
 import { FC } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './RegistrationForm.css';
 
-const RegistrationForm: FC = () => (
-  <div className="registration-form-container">
-    <h1>登録フォーム</h1>
-    <form>
-      <label htmlFor="name">
-        名前
-        <input type="text" id="name" />
-      </label>
-      <label htmlFor="email">
-        メールアドレス
-        <input type="email" id="email" />
-      </label>
-      <button type="submit">送信</button>
-    </form>
-  </div>
-);
+type FormData = {
+  name: string;
+  email: string;
+};
+
+const RegistrationForm: FC = () => {
+  const { register, handleSubmit } = useForm<FormData>({
+    defaultValues: {
+      name: '',
+      email: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
+
+  return (
+    <div className="registration-form-container">
+      <h1>登録フォーム</h1>
+      <form onSubmit={handleSubmit(onSubmit)} action="/">
+        <label htmlFor="name">
+          名前
+          <input type="text" id="name" {...register('name')} />
+        </label>
+        <label htmlFor="email">
+          メールアドレス
+          <input type="email" id="email" {...register('email')} />
+        </label>
+        <button type="submit">送信</button>
+      </form>
+    </div>
+  );
+};
 
 export default RegistrationForm;
