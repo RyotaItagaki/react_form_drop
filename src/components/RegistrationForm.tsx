c33b6cfcbe260837fc7444892237f6bc257f6637
Ryota Itagaki <ribitamana@gmail.com>
2022-11-27 23:32:09 +0900

    react-dropzoneでドラッグ&ドロップを実現

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,21 +1,52 @@
 /* eslint-disable no-console */
 /* eslint-disable react/jsx-props-no-spreading */
-import { FC } from 'react';
+import { FC, useCallback, useMemo } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useDropzone } from 'react-dropzone';
 import './RegistrationForm.css';
 
 type FormData = {
   name: string;
   email: string;
+  file: File | null;
 };
 
 const RegistrationForm: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>({
+  const { register, handleSubmit, setValue, watch } = useForm<FormData>({
     defaultValues: {
       name: '',
       email: '',
+      file: null,
     },
   });
+
+  const onDrop = useCallback((files: File[]) => {
+    if (files.length > 0) {
+      setValue('file', files[0]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      'image/png': ['.png', '.jpg', '.jpeg'],
+    },
+  });
+
+  const dropAreaBackground = isDragActive ? 'gray' : '';
+
+  const watchFile = watch('file');
+
+  const filePreview = useMemo(() => {
+    if (!watchFile) {
+      return <></>;
+    }
+
+    const url = URL.createObjectURL(watchFile);
+
+    return <img src={url} alt="" className="file-preview" />;
+  }, [watchFile]);
 
   const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
 
@@ -31,6 +62,16 @@ const RegistrationForm: FC = () => {
           メールアドレス
           <input type="email" id="email" {...register('email')} />
         </label>
+        <p className="drop-area-title">プロフィール画像</p>
+        <div {...getRootProps()} className={`drop-area ${dropAreaBackground}`}>
+          <input {...getInputProps} />
+          <p>
+            ファイルを選択または
+            <br />
+            ドラッグアンドドロップ
+          </p>
+        </div>
+        {filePreview}
         <button type="submit">送信</button>
       </form>
     </div>
